9e7d8a4e95b7b23146294ad0a55ff13c7915b23e
enocklennox <lennoxkipngeno@gmail.com>
2023-03-22 13:05:39 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_sign - prints the sign of a number
  * @n: number to check
@@ -10,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
