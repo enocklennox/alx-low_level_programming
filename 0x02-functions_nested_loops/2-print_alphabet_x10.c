bdca79ea997df9f8e9486ca3db110f0654e62982
enocklennox <lennoxkipngeno@gmail.com>
2023-03-22 08:51:24 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,23 +5,28 @@
  *
  * Return: void
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+/**
+ *print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-			_putchar('\n');
-	}
-}
-
-int main(void)
-{
 	print_alphabet_x10();
-	return (0);
+	}
 }
