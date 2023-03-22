c6d6c3148f8215ccd4143ec6af8bf54c7057b62a
enocklennox <lennoxkipngeno@gmail.com>
2023-03-22 08:40:20 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
