377274aa2c520f3159cfec7e6306c93a908b7b07
enocklennox <lennoxkipngeno@gmail.com>
2023-04-06 03:02:22 +0300

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,37 @@
-#include <main.h>
+#include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 /**
- *_sqrt_recursion -> sqrt using recursion
- *@n: parameter to be sqrt
- *Return: square root of a number
+ *find_sqrt - Finds the natural square root of an inputted number
+ *@num: The number to find the square root of
+ *@root: The root to be tested.
+ *
+ *Return: If the number has a natural square root - the square root.
+ *If the number does not have a natural square root - -1.
  */
-
-int helperFunction(int num, int pSqrt)
+int find_sqrt(int num, int root)
 {
-	if ((pSqrt * pSqrt) == num)
-	{
-		return (pSqrt);
-	}
-	else
-	{
-		if ((pSqrt * pSqrt) >num)
-			return (-1);
-		else
-			return (helperFunction(num, pSqrt + 1));
-	}	
+	if ((root * root) == num)
+		return (root);
+	if (root == num / 2)
+		return (-1);
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * _sqrt_recursion -returns the natural square root of a number.
- * @n: number to find sqrt of
+ *_sqrt_recursion - Returns the natural square root of a number.
+ *@n: The number to return the square root of.
  *
- * Return: squareroot of n.
- * -1 if n does not have a natural sqrt
+ *Return: If n has a natural square root - the natural square root of n.
+ *If n does not have a natural square root - -1.
  */
 int _sqrt_recursion(int n)
 {
+	int root = 0;
+
 	if (n < 0)
 		return (-1);
-	else
-		return (helperFunction(n, 0));
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
 }
