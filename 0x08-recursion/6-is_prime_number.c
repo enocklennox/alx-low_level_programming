f7d3f572b938e17bf054650adfcf37c2196325af
enocklennox <lennoxkipngeno@gmail.com>
2023-04-06 03:12:28 +0300

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,73 +1,21 @@
-/**
- *is_prime - checks if a number is prime
- *@n: the number to check
- *
- *Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(int n)
-{
-	int i;
+#include "main.h"
 
-	if (n <= 1)
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ *is_divisible - Checks if a number is divisible.
+ *@num: The number to be checked.
+ *@div: The divisor.
+ *
+ *Return: If the number is divisible - 0.
+ *If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
 		return (0);
-
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
-}
-
-/**
- *main - entry point
- *
- *Return: Always 0
- */
-int main(void)
-{
-	int n;
-
-	n = get_number();
-	if (is_prime(n))
-		print_is_prime(n);
-	else
-		print_not_prime(n);
-
-	return (0);
-}
-
-/**
- *get_number - prompts user for an integer
- *
- *Return: integer entered by user
- */
-int get_number(void)
-{
-	char buffer[1024];
-	int n;
-
-	printf("Enter an integer: ");
-	fgets(buffer, sizeof(buffer), stdin);
-	sscanf(buffer, "%d", &n);
-
-	return (n);
-}
-
-/**
- *print_is_prime - prints that a number is prime
- *@n: the number to print
- */
-void print_is_prime(int n)
-{
-	printf("%d is a prime number.\n", n);
-}
-
-/**
- *print_not_prime - prints that a number is not prime
- *@n: the number to print
- */
-void print_not_prime(int n)
-{
-	printf("%d is not a prime number.\n", n);
+	if (div == num / 2)
+		return (1);
+	return (is_divisible(num, div + 1));
 }
