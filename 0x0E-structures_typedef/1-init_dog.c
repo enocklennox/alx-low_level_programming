1927b08fbc8a700334d7799757ac783ea46654cb
enocklennox <lennoxkipngeno@gmail.com>
2023-04-17 21:53:52 +0300

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d: pointer to a struct dog
@@ -10,10 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
 }
