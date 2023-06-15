2a35cf0ad65393c55fe73cded50b6cbb716a4844
enocklennox <lennoxkipngeno@gmail.com>
2023-06-15 21:00:26 +0300

    list.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,14 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
- */
+
+
+
 typedef struct dlistint_s
 {
 	int n;
