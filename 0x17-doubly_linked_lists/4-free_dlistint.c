0b0fdc5726837fed3437e267993bf6dd2a7d12e6
enocklennox <lennoxkipngeno@gmail.com>
2023-06-16 00:30:00 +0300

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
+
